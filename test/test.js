55bbfd8d358ab1a8e510ecd76029014e3665e920
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-24 18:46:21 +0300

    Setup tests

---
+++ test/test.js
@@ -0,0 +1,6 @@
+require.config({
+  paths: {
+    'src': '../src'
+  }
+});
+require(['views/carousel'], jasmineRun);
---
+++ test/views/carousel.js
@@ -0,0 +1,8 @@
+var resource = 'src/views/carousel';
+define([resource], function (CarouselView) {
+  describe(resource, function () {
+    it('should be defined', function () {
+      expect(CarouselView).toBeDefined();
+    });
+  });
+});
