a329aa70ac380b0543e3a5fa57aff4a5bef8878b
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-24 19:32:28 +0300

    Create base carousel layout

--- src/views/carousel.js
+++ src/views/carousel.js
@@ -1,19 +1,57 @@
 define([], function () {
   var CarouselView = function CarouselView() {
-    this.el = document.createElement('div');
+    this.el = null;
+
+    this.btnNext = null;
+    this.btnPrev = null;
+
+    this.viewBox = null;
+
+    this.slidesList = null;
   };
 
   CarouselView.prototype.classNames = {
-    el: 'carousel'
+    el: 'carousel',
+    btnNext: 'carousel-btn-next',
+    btnPrev: 'carousel-btn-prev',
+    viewBox: 'carousel-view-box',
+    slidesList: 'carousel-slides-list'
+  };
+
+  CarouselView.prototype.createLayout = function () {
+    this.el = document.createElement('div');
+
+    this.btnNext = document.createElement('button');
+    this.btnPrev = document.createElement('button');
+
+    this.viewBox = document.createElement('div');
+
+    this.slidesList = document.createElement('ul');
+    this.viewBox.appendChild(this.slidesList);
+
+    this.el.appendChild(this.btnNext);
+    this.el.appendChild(this.viewBox);
+    this.el.appendChild(this.btnPrev);
+    return this;
   };
 
   CarouselView.prototype.assignClassNames = function () {
-    this.el.className = this.classNames.el;
+    var ownPropertyNames = Object.getOwnPropertyNames(this);
+
+    ownPropertyNames.forEach(function (propName) {
+      var node = this[propName];
+      var expectedClass = this.classNames[propName];
+
+      if (typeof expectedClass === 'string') {
+        node.className = expectedClass;
+      }
+    }.bind(this));
+
     return this;
   };
 
   CarouselView.prototype.render = function () {
-    return this.assignClassNames();
+    return this.createLayout().assignClassNames();
   };
 
   return CarouselView;
--- test/views/carousel.js
+++ test/views/carousel.js
@@ -1,8 +1,62 @@
 var resource = 'src/views/carousel';
+
+var expectToCreateDOMElement = function (node) {
+  expect(node).toBeDefined();
+  expect(node instanceof HTMLElement).toBe(true);
+};
+
 define([resource], function (CarouselView) {
   describe(resource, function () {
+    beforeEach(function () {
+      this.carouselView = new CarouselView();
+    });
+
     it('should be defined', function () {
       expect(CarouselView).toBeDefined();
+    });
+
+    describe('.createLayout', function () {
+      beforeEach(function () {
+        this.carouselView.createLayout();
+      });
+
+      it('creates a root element of the view', function () {
+        expectToCreateDOMElement(this.carouselView.el);
+      });
+
+      it('creates next button', function () {
+        expectToCreateDOMElement(this.carouselView.btnNext);
+      });
+
+      it('creates previous button', function () {
+        expectToCreateDOMElement(this.carouselView.btnPrev);
+      });
+
+      it('creates viewBox', function () {
+        expectToCreateDOMElement(this.carouselView.viewBox);
+      });
+
+      it('creates slidesList', function () {
+        expectToCreateDOMElement(this.carouselView.slidesList);
+      });
+    });
+
+    describe('.assignClassNames', function () {
+      beforeEach(function () {
+        this.carouselView.createLayout().assignClassNames();
+      });
+
+      it('assigns correct classNames to elements', function () {
+        var ownProps = Object.getOwnPropertyNames(this.carouselView);
+        ownProps.forEach(function (propName) {
+          var property = this.carouselView[propName];
+          var expectedClassName = this.carouselView.classNames[propName] || '';
+
+          if (property instanceof HTMLElement && expectedClassName) {
+            expect(property.className).toEqual(expectedClassName);
+          }
+        }.bind(this));
+      });
     });
   });
 });
