f41638e7328769912a23cff4ee27fa4716696ef1
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-26 15:22:46 +0300

    Fix CarouselModel class definition

--- src/models/Carousel.js
+++ src/models/Carousel.js
@@ -2,7 +2,7 @@ define(['src/utils'], function (utils) {
   var defaultConfig = {
     items: []
   };
-  var CarouselModel = function (config) {
+  var CarouselModel = function CarouselModel(config) {
     config = utils.isObject(config) ? config : defaultConfig;
 
     this.config = utils.isObject(config) || {};
