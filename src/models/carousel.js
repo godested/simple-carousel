2144e6324ae9a482bf138c4f7021e758d01b4fcb
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-26 13:34:55 +0300

    Create default setting for carousel model

--- src/models/carousel.js
+++ src/models/carousel.js
@@ -1,7 +1,12 @@
-define(['utils'], function (utils) {
+define(['src/utils'], function (utils) {
+  var defaultConfig = {
+    items: []
+  };
   var CarouselModel = function (config) {
+    config = utils.isObject(config) ? config : defaultConfig;
+
     this.config = utils.isObject(config) || {};
-    this.items = [];
+    this.items = config.items;
   };
 
   return CarouselModel;
