ac61d3b14a350fa229d5f3f685f05aa2fc81315f
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-26 13:35:25 +0300

    Fix settings checking for carousel view. add model to the defaults

--- src/views/carousel.js
+++ src/views/carousel.js
@@ -1,50 +1,53 @@
-define(['text!src/templates/carousel.html', 'src/utils'], function (carouselTemplate, utils) {
-  var defaultSettings = {
-    template: carouselTemplate
-  };
+define(['src/models/carousel', 'text!src/templates/carousel.html', 'src/utils'],
+    function (CarouselModel, carouselTemplate, utils) {
+      var defaultSettings = {
+        template: carouselTemplate,
+        model: new CarouselModel()
+      };
 
-  var CarouselView = function CarouselView(settings) {
-    settings = utils.isObject(settings) || defaultSettings;
+      var CarouselView = function CarouselView(settings) {
+        settings = utils.isObject(settings) ? settings : defaultSettings;
 
-    this.el = null;
+        this.el = null;
 
-    this.btnNext = null;
-    this.btnPrev = null;
+        this.btnNext = null;
+        this.btnPrev = null;
 
-    this.viewBox = null;
+        this.viewBox = null;
 
-    this.slidesList = null;
+        this.slidesList = null;
 
-    this.template = settings.template;
-  };
+        this.template = settings.template;
+        this.model = settings.model;
+      };
 
-  CarouselView.prototype.classNames = {
-    el: 'carousel'
-  };
+      CarouselView.prototype.classNames = {
+        el: 'carousel'
+      };
 
-  CarouselView.prototype.createLayout = function () {
-    this.el = document.createElement('div');
-    this.el.className = this.classNames.el;
+      CarouselView.prototype.createLayout = function () {
+        this.el = document.createElement('div');
+        this.el.className = this.classNames.el;
 
-    this.el.innerHTML = this.template;
+        this.el.innerHTML = this.template;
 
 
-    return this.collectChildNodes();
-  };
+        return this.collectChildNodes();
+      };
 
-  CarouselView.prototype.collectChildNodes = function () {
-    this.btnNext = this.el.querySelector('.carousel-btn-next');
-    this.btnPrev = this.el.querySelector('.carousel-btn-prev');
+      CarouselView.prototype.collectChildNodes = function () {
+        this.btnNext = this.el.querySelector('.carousel-btn-next');
+        this.btnPrev = this.el.querySelector('.carousel-btn-prev');
 
-    this.viewBox = this.el.querySelector('.carousel-view-box');
-    this.slidesList = this.el.querySelector('.carousel-view-box');
+        this.viewBox = this.el.querySelector('.carousel-view-box');
+        this.slidesList = this.el.querySelector('.carousel-view-box');
 
-    return this;
-  };
+        return this;
+      };
 
-  CarouselView.prototype.render = function () {
-    return this.createLayout();
-  };
+      CarouselView.prototype.render = function () {
+        return this.createLayout();
+      };
 
-  return CarouselView;
-});
+      return CarouselView;
+    });
