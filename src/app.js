1f431c8cad146f520f5d2da2602e014bf9cdeae3
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-27 12:57:31 +0300

    Add carousel items initialization at app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 define(['views/carousel'], function (CarouselView) {
   var carousel = new CarouselView();
   var container = document.querySelector('.container');
-
+  
+  for (var i = 1; i <= 5; i++) {
+    carousel.model.addItem('http://via.placeholder.com/350x350?text=placeholder ' + i);
+  }
+  
   return {
     start: function () {
       if (container !== null) {
