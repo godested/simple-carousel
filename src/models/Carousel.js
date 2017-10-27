32d1509779a6fae78dd25188a4096be627de54b4
Igor Bondar <ihor.bondar92@gmail.com>
2017-10-27 13:20:55 +0300

    Fix CarouselModel default config

--- src/models/Carousel.js
+++ src/models/Carousel.js
@@ -1,19 +1,19 @@
 define(['src/util/Observable', 'src/utils', 'src/errors'], function (Observable, utils, errors) {
-  var defaultConfig = {
-    items: []
+  var getDefaultConfig = function () {
+    return {
+      items: []
+    };
   };
-  
+
   var CarouselModel = function CarouselModel(config) {
     CarouselModel.superClass.call(this);
-    
-    config = utils.isObject(config) ? config : defaultConfig;
-    
-    this.config = utils.isObject(config) || {};
+
+    config = utils.isObject(config) ? config : getDefaultConfig();
     this.items = config.items;
   };
-  
+
   utils.extend(CarouselModel, Observable);
-  
+
   Object.defineProperty(CarouselModel.prototype, 'items', {
     get: function () {
       return this._items;
@@ -22,19 +22,19 @@ define(['src/util/Observable', 'src/utils', 'src/errors'], function (Observable,
       if (!Array.isArray(value)) {
         throw new TypeError(errors.TYPE_ERROR.ARRAY_EXPECTED)
       }
-      
+
       this._items = value;
       this.setChanged().notifyObservers(this.items);
     }
   });
-  
+
   CarouselModel.prototype.addItem = function (item) {
     this.items.push(item);
-    
+
     this.setChanged().notifyObservers(this.items);
-    
+
     return this;
   };
-  
+
   return CarouselModel;
 });
